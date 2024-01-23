b7436954e50d02de6ac55efac73ec9484c369f02
ahmadadnan16 <ahmadadnan1602@gmail.com>
2024-01-23 21:03:38 +0530

    Added About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='my-5'>
+    <h1 className="text-purple-700 w-11/12 mx-auto text-xl md:2xl lg:text-3xl uppercase font-semibold  py-7">About Us</h1>
+      <div className='w-11/12 mx-auto text-black/70 bg-white border border-gray-200 rounded-lg shadow p-3'>
+        <p className='text-xl leading-8 py-5 text-justify'><span className='font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span> Obcaecati odio iste consequuntur adipisci deserunt dicta repellat quibusdam, optio a officiis accusamus ipsum omnis ut voluptates maiores veritatis id magnam ullam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati odio iste consequuntur adipisci deserunt dicta repellat quibusdam, optio a officiis accusamus ipsum omnis ut voluptates maiores veritatis id magnam ullam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati odio iste consequuntur adipisci deserunt dicta repellat quibusdam, optio a officiis accusamus ipsum omnis ut voluptates maiores veritatis id magnam ullam.</p>
+        <p className='text-xl leading-8 py-5 text-justify'><span className='font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</span> Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati odio iste consequuntur adipisci deserunt dicta repellat quibusdam, optio a officiis accusamus ipsum omnis ut voluptates maiores veritatis id magnam ullam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati odio iste consequuntur adipisci deserunt dicta repellat quibusdam, optio a officiis accusamus ipsum omnis ut voluptates maiores veritatis id magnam ullam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati odio iste consequuntur adipisci deserunt dicta repellat quibusdam, optio a officiis accusamus ipsum omnis ut voluptates maiores veritatis id magnam ullam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati odio iste consequuntur adipisci deserunt dicta repellat quibusdam, optio a officiis accusamus ipsum omnis ut voluptates maiores veritatis id magnam ullam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati odio iste consequuntur adipisci deserunt dicta repellat quibusdam, optio a officiis accusamus ipsum omnis ut voluptates maiores veritatis id magnam ullam.Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati odio iste consequuntur adipisci deserunt dicta repellat quibusdam, optio a officiis accusamus ipsum omnis ut voluptates maiores veritatis id magnam ullam.</p>
+      </div>
+    </div>
   )
 }
 
