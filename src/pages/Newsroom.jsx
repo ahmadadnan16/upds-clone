6c4b1cb570cc9d15273d990931d33e2429e59b80
ahmadadnan16 <ahmadadnan1602@gmail.com>
2024-02-05 15:18:59 +0530

    Added newsroom page

--- src/pages/Newsroom.jsx
+++ src/pages/Newsroom.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import NewsCard from '../components/NewsCard'
+import NewsArchive from '../components/NewsArchive'
 
 const Newsroom = () => {
   return (
-    <div>Newsroom</div>
+    <div className='lg:flex my-10'>
+    <div className='w-full lg:w-10/12'>
+    <NewsCard/>
+    <NewsCard/>
+    <NewsCard/>
+    <NewsCard/>
+    </div>
+    <div className='w-2/12 hidden lg:block'>
+    <NewsArchive/>
+
+    </div>
+    </div>
   )
 }
 
